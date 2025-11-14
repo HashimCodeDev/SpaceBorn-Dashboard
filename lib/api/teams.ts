53969e5d8b838713ea812649ce46c267327d4a11
Hashim <hashimmohammed212@gmail.com>
2025-11-14 16:48:14 +0530

    feat:created teams api

---
+++ lib/api/teams.ts
@@ -0,0 +1,29 @@
+import { api } from "../apiBase";
+
+export function listTeams() {
+    return api("teams/");
+}
+
+export function getTeam(id: number) {
+    return api(`teams/${id}/`);
+}
+
+export function createTeam(body: any) {
+    return api("teams/", {
+        method: "POST",
+        body: JSON.stringify(body),
+    });
+}
+
+export function updateTeam(id: number, body: any) {
+    return api(`teams/${id}/`, {
+        method: "PUT",
+        body: JSON.stringify(body),
+    });
+}
+
+export function deleteTeam(id: number) {
+    return api(`teams/${id}/`, {
+        method: "DELETE",
+    });
+}
