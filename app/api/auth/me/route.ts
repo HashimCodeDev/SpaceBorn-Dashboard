da2841e30b13b63e60975863c4c773679e0a65cf
Hashim <hashimmohammed212@gmail.com>
2025-11-14 11:42:01 +0530

    feat:added routing for auth/me

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -7,10 +7,13 @@ export async function GET() {
 
     if (!token) return NextResponse.json({ user: null }, { status: 401 });
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/me/`, {
+    const baseUrl = process.env.API_URL;
+
+    const res = await fetch(`${baseUrl}/api/me/`, {
         headers: { Cookie: `token=${token}` },
         credentials: 'include',
     });
+
 
     if (!res.ok) return NextResponse.json({ user: null }, { status: res.status });
 
